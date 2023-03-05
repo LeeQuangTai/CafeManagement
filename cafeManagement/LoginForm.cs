cb2f2695ec9579ac946f8ca7764a72191d96e26f
LeeQuangTai <90539363+LeeQuangTai@users.noreply.github.com>
2023-03-05 13:30:19 +0700

    Initial commit

---
+++ cafeManagement/LoginForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafeManagement
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void signInLabel_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Clicked");
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
